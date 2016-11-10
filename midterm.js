76bb9b675cfd6c4fa541a5f374b6433d5ff105e2
xg2242 <xg2242@tc.columbia.edu>
2016-11-09 22:38:16 -0500

    change

---
+++ midterm.js
@@ -0,0 +1,12 @@
+document.getElementById('left button').addEventListener('click', function() {
+    alert('Too concrete! All I words will not move your audience!');
+});
+
+document.getElementById('left button').addEventListener('click', function() {
+    alert('Hello World!');
+});
+
+function Submit() {
+    var x = document.getElementById("myText").value;
+    document.getElementById("Submitted").innerHTML = x;
+}
